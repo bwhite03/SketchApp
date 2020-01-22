2d3142d545e08900acadfe53e0e58dbeaa15eb92
bwhite03 <bwhite1195@yahoo.com>
2020-01-22 18:41:52 -0500

    added eraser

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,18 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 const clearButton = document.querySelector(".clear");
+const eraseButton = document.querySelector(".eraser");
 const strokeWeight = document.querySelector(".stroke-weight");
 const thickness = document.querySelector(".thickness-click");
 const parent = document.querySelector("#picker");
 const picker = new Picker(parent);
 let isDrawing = false;
+let isErasing = false;
+let currentColor;
 
 picker.onChange = color => {
   parent.style.background = color.rgbaString;
-  ctx.strokeStyle = color.rgbaString;
+  currentColor = ctx.strokeStyle = color.rgbaString;
 };
 
 const start = e => {
@@ -47,6 +50,16 @@ thickness.addEventListener("click", () => {
     strokeWeight.style.display = "none";
   } else {
     strokeWeight.style.display = "block";
+  }
+});
+
+eraseButton.addEventListener("click", () => {
+  isErasing = !isErasing;
+
+  if (isErasing === true) {
+    ctx.strokeStyle = "#f3f3f3";
+  } else {
+    ctx.strokeStyle = currentColor;
   }
 });
 
